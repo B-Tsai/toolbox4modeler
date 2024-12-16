7e9671562c3e4b15147dd865df7b44bc9af208b2
Benjamin Tsai <benjamintsai@usgs.gov>
2024-12-16 11:18:48 -0500

    Several updates

--- toolbox4modeler/mpre.py
+++ toolbox4modeler/mpre.py
@@ -3,10 +3,11 @@ Functions for model pre-processing
 
 """
 import numpy as np
+import os
 
 
 
-def sw_bthy_inp(xb, zb, h, x, outp_dir, file_name):
+def sw_bthy_inp(xb, zb, h0, x, outp_dir, file_name):
     """
     Generate a user-defined input file of bathymetry for SWASH.
     
@@ -36,25 +37,24 @@ def sw_bthy_inp(xb, zb, h, x, outp_dir, file_name):
     Raises:
             
     """
-    import os
     from scipy import interpolate
     
     # Preparation
     outp = os.path.join(outp_dir, file_name)
     interp1 = interpolate.interp1d(xb, zb, bounds_error=False, fill_value=np.nan)
     z = interp1(x)
-    bot = h - z
-    con = ['']    
-    for i in range(len(bot)):
-       con[0] = con[0] + ('{:.6e}\n'.format(bot[i]))
+    h = h0 - z
+    con = ''
+    for i in range(len(h)):
+       con = con + ('{:.6e}\n'.format(h[i]))
 
     # Output
     with open(outp, "w") as file:
-        file.write(con[0])
+        file.write(con)
 
 
 
-def sw_wave_inp(a0, a, w, ps, outp_dir, file_name):
+def sw_wave_inp(a0, a, w, phs, outp_dir, file_name):
     """
     Generate a user-defined input file of wave spectrum for SWASH.
     
@@ -70,7 +70,7 @@ def sw_wave_inp(a0, a, w, ps, outp_dir, file_name):
         w : 1-D ndarray of float
             Wave angular frequency [rad/s] of each wave component (w = 2*pi/T).
                 
-        ps : 1-D ndarray of float
+        phs : 1-D ndarray of float
             Phase shift [deg] of each wave component.
         
         outp_dir : str
@@ -84,16 +84,173 @@ def sw_wave_inp(a0, a, w, ps, outp_dir, file_name):
     Raises:
             
     """
-    import os
     
     # Preparation
     outp = os.path.join(outp_dir, file_name)
-    con = ('{:.6e} &\n'.format(a0))
-    #con = ['']
+    con = ''
     for i in range(len(w)):
-       con[0] = con[0] + ('{:.6e} {:.6e} {:11.6f} &\n'.format(a[i],w[i],ps[i]))
+        con = con + ('{:.6e} {:.6e} {:11.6f} &\n'.format(a[i],w[i],phs[i]))
 
     # Output
     with open(outp, "w") as file:
-        file.write('BOU SIDE W CCW BTYPE WEAK SMOO 1 SEC CON FOUR ' + con[0])
-        #file.write('BOU SIDE W CCW BTYPE WEAK SMOO 1 SEC CON FOUR 0 &\n' + con[0])
+        file.write('BOU SIDE W CCW BTYPE WEAK SMOO 1 SEC CON FOUR ' + '{:.6e} &\n'.format(a0) + con)
+
+
+
+def cr_bthy_inp(xb, zb, h0, x, outp_dir, file_name):
+    """
+    Generate a user-defined input file of bathymetry for CROCO.
+    
+    References:
+        
+    Parameters:
+        xb : 1-D ndarray of float
+            x coordinate of the raw bathymetry [m].
+            
+        zb : 1-D ndarray of float
+            z coordinate of the raw bathymetry [m].
+
+        h : float
+            Water depth [m].
+                
+        x : 1-D ndarray of float
+            x coordinate of the model domain [m].
+        
+        outp_dir : str
+            Directory of the output file
+            
+        file_name : str
+            File name of the output file
+            
+    Returns:
+
+    Raises:
+            
+    """
+    from datetime import datetime
+    from scipy import interpolate
+    
+    import netCDF4 as nc
+    
+    # Preparation
+        
+    nx = len(x) - 2
+    ny = 1
+    dx = np.mean(np.diff(x))
+    dy = dx
+    size_x = nx*dx
+    size_y = ny*dy
+    y = np.linspace(-dy, dy, ny+2)
+    x_rho, y_rho = np.meshgrid(x, y)
+    interp1 = interpolate.interp1d(xb, zb, bounds_error=False, fill_value=np.nan)
+    z0 = interp1(x)
+    z = np.reshape(np.tile(z0, ny+2), [ny+2,-1])
+    h = h0 - z
+    
+
+    # Output
+    # Create and save a new CROCO grid file
+    path_grd = os.path.join(outp_dir, file_name+".nc")
+    nc_grd = nc.Dataset(path_grd, 'w', format='NETCDF4')
+    nc_grd.created = datetime.now().isoformat()
+    nc_grd.type = 'CROCO grid file produced by prep_croco_case.py'
+
+    # create dimensions
+    nc_grd.createDimension('one', 1)
+    nc_grd.createDimension('xi_rho', nx + 2)
+    nc_grd.createDimension('eta_rho', ny + 2)
+    nc_grd.createDimension('xi_u', nx + 1)
+    nc_grd.createDimension('eta_v', ny + 1)
+    nc_grd.createDimension('xi_psi', nx + 1)
+    nc_grd.createDimension('eta_psi', ny + 1)
+    
+    nc_grd.createVariable('xl', 'f8', ('one'))
+    nc_grd.variables['xl'].long_name = 'Domain length in the XI-direction'
+    nc_grd.variables['xl'].units = 'm'
+    nc_grd.variables['xl'][:] = size_x
+
+    nc_grd.createVariable('el', 'f8', ('one'))
+    nc_grd.variables['el'].long_name = 'Domain length in the ETA-direction'
+    nc_grd.variables['el'].units = 'm'
+    nc_grd.variables['el'][:] = size_y
+
+    # create variables and attributes
+    nc_grd.createVariable('spherical', 'S1', ('one'))
+    nc_grd.variables['spherical'].long_name = 'Grid type logical switch'
+    nc_grd.variables['spherical'].option_T = 'spherical'
+    nc_grd.variables['spherical'][:] = 'F'
+
+    nc_grd.createVariable('h', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['h'].long_name = 'Bottom depth at RHO-points.'
+    nc_grd.variables['h'].units = 'm'
+    nc_grd.variables['h'][:] = h
+
+    nc_grd.createVariable('f', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['f'].long_name = 'Coriolis parameter'
+    nc_grd.variables['f'].units = 's-1'
+    nc_grd.variables['f'][:] = np.zeros(h.shape)
+
+    nc_grd.createVariable('pm', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['pm'].long_name = 'Coordinate transformation metric "m" associated with the differential distances in XI.'
+    nc_grd.variables['pm'].units = 'm-1'
+    nc_grd.variables['pm'][:] = nx/size_x*np.ones(h.shape)
+
+    nc_grd.createVariable('pn', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['pn'].long_name = 'Coordinate transformation metric "n" associated with the differential distances in ETA.'
+    nc_grd.variables['pn'].units = 'm-1'
+    nc_grd.variables['pn'][:] = ny/size_y*np.ones(h.shape)
+
+    nc_grd.createVariable('x_rho', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['x_rho'].long_name = 'XI-coordinates at RHO-points.'
+    nc_grd.variables['x_rho'].units = 'm'
+    nc_grd.variables['x_rho'][:] = x_rho
+
+    nc_grd.createVariable('y_rho', 'f8', ('eta_rho', 'xi_rho'))
+    nc_grd.variables['y_rho'].long_name = 'ETA-coordinates at RHO-points.'
+    nc_grd.variables['y_rho'].units = 'm'
+    nc_grd.variables['y_rho'][:] = y_rho
+
+    nc_grd.close()
+
+
+
+def cr_wave_inp(a, w, phs, k, outp_dir, file_name):
+    """
+    Generate a user-defined input file of wave spectrum for CROCO.
+    
+    References:
+        
+    Parameters:            
+        a : 1-D ndarray of float
+            Wave amplitude [m] of each wave component (a = H/2).
+            
+        w : 1-D ndarray of float
+            Wave angular frequency [rad/s] of each wave component (w = 2*pi/T).
+                
+        phs : 1-D ndarray of float
+            Phase shift [rad] of each wave component.
+            
+        k : 1-D ndarray of float
+            Wave number [rad/m] of each wave component (k = 2*pi/L).
+        
+        outp_dir : str
+            Directory of the output file
+            
+        file_name : str
+            File name of the output file
+            
+    Returns:
+
+    Raises:
+            
+    """
+    
+    # Preparation
+    outp = os.path.join(outp_dir, file_name)
+    con = ''
+    for i in range(len(w)):
+        con = con + ('{:.6e} {:.6e} {:.6e} {:.6e}\n'.format(a[i],w[i],phs[i],k[i]))
+
+    # Output
+    with open(outp, "w") as file:
+        file.write(con)        
--- toolbox4modeler/wave.py
+++ toolbox4modeler/wave.py
@@ -75,3 +75,115 @@ def disper(w, h, g=9.80665, tol=1e-9, maxiter=100):
     
     return k
 
+
+
+def cnoidal_wave(h, H, T, t, x, g=9.80665, tol=1e-9):
+    """
+    Calculate the surface elevation time series of a cnoidal wave, using the 
+    approximation of decimals method.
+    
+    Parameters:
+        h : float
+            Water depth [m].
+
+        H : float
+            Wave height [m].
+
+        T : float
+            Wave period [s].
+
+        t : array_like
+            Desired output time [s].            
+
+        x : float
+            Desired output location [m].
+ 
+        g : float, optional
+            Acceleration of gravity [m^2 s^-2]. Default: 9.80665.
+
+        tol : float, optional
+            Tolerance [-] for termination. Default: 1e-9.
+
+    Returns:
+        wl : ndarray
+            Time series of the surface elevation  [m]. An array with the same 
+            size as t.      
+
+        L : float
+            Wave length [m].
+
+        c : float
+            Wave celerity [m/s].
+            
+        m: float
+            Elliptic parameter [-].
+
+    Raises:
+            
+    """
+    from scipy import special
+    
+    # Approximate m to the desired tolerence
+    m0 = 0.5
+    dm = 0.1
+    m = np.arange(m0, 1+dm/10, dm)
+    while dm >= tol:
+        K = special.ellipk(m)
+        E = special.ellipe(m)
+        c = np.sqrt(g*h)*(1+H/(m*h)*(1-m/2-3/2*E/K))
+        L = h*np.sqrt(16/3*m*h/H*c/np.sqrt(g*h))*K
+        err = L-c*T;
+        m0 = m[err<0]
+        dm = dm/10
+        m = np.arange(m0[-1], m0[-1]+dm*10.1, dm)
+    idx = np.argmin(np.abs(err))
+    K = K[idx]
+    E = E[idx]
+    L = L[idx]
+    c = c[idx]
+    m = m[idx]
+    
+    # Calculate time series
+    wl2 = H/m*(1-m-E/K)
+    _, cn, _, _ = special.ellipj(2*K*(x-c*t)/L, m)
+    wl = wl2 + H*cn**2
+    
+    return wl, L, c, m
+
+
+
+def fifth_stokes_wave(h, a, T, g=9.80665):
+    """
+    Calculate the wave component of a 5th order Stokes wave.
+    
+    Parameters:
+        h : float
+            Water depth [m].
+
+        a : float
+            Wave amplitude [m].
+
+        T : float
+            Wave period [s].
+ 
+        g : float, optional
+            Acceleration of gravity [m^2 s^-2]. Default: 9.80665.
+
+    Returns:
+        wl : ndarray
+            Time series of the surface elevation  [m]. An array with the same 
+            size as t.      
+
+        L : float
+            Wave length [m].
+
+        c : float
+            Wave celerity [m/s].
+            
+        m: float
+            Elliptic parameter [-].
+
+    Raises:
+            
+    """
+    
